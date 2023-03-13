246261c909ebc324bc47d178aadfca935b1fcc4f
scornea <scornea3@gmail.com>
2023-03-13 11:09:57 +0000

    fifty-shades-of-cold

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,4 +1,5 @@
 import { colors } from "./fifty-shades-of-cold.data.js";
+
 function generateClasses() {
     const head = document.getElementsByTagName("head")[0];
     const style = document.createElement("style");
@@ -8,6 +9,7 @@ function generateClasses() {
     console.log(style.innerHTML);
     head.appendChild(style);
 }
+
 function generateColdShades() {
     const body = document.getElementsByTagName("body")[0];
     colors.forEach((color) => {
@@ -21,9 +23,11 @@ function generateColdShades() {
         }
     });
 }
+
 function choseShade(shade) {
     document.querySelectorAll("div").forEach((div) => {
         div.className = shade;
     });
 }
+
 export { generateClasses, generateColdShades, choseShade };
