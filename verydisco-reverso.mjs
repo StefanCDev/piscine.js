e633f0b6c43367db9af29aa72cef73dc8bcc28f3
scornea <scornea3@gmail.com>
2023-03-23 21:26:38 +0000

    verydisco-reverso

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,20 +1,16 @@
 import { readFileSync } from 'fs';
 
-// Get the file name from the command-line arguments
-const fileName = process.argv[2];
-
-// Read the content of the file
-const content = readFileSync(fileName, 'utf-8');
-
-// Reverse the content from the very disco mode
 const discoChars = 'DSECORYIUAPFGHLJKLMNBQTVWXZ';
 const plainChars = 'DISCOVERYABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const reverseMap = new Map([...discoChars].map((c, i) => [c, plainChars[i]]));
-const reversedContent = content
-  .split('')
-  .map(c => reverseMap.has(c) ? reverseMap.get(c) : c)
-  .reverse()
-  .join('');
 
-// Print the reversed content to the console
-console.log(reversedContent);
+const reverseVeryDisco = content =>
+  content
+    .split('')
+    .map(c => reverseMap.has(c) ? reverseMap.get(c) : c)
+    .reverse()
+    .join('');
+
+const filePath = process.argv[2];
+const content = readFileSync(filePath, 'utf8');
+console.log(reverseVeryDisco(content));
