ce5d9899108e1e26c03925d8b71cac5e54ff6103
scornea <scornea3@gmail.com>
2023-03-23 21:09:36 +0000

    verydisco-reverso

---
+++ verydisco-reverse.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+
+const args = process.argv.slice(2);
+
+function reverseVeryDisco(str) {
+  return str.split(' ')
+            .map(word => {
+              const mid = Math.ceil(word.length / 2);
+              const firstHalf = word.slice(0, mid);
+              const secondHalf = word.slice(mid);
+              return secondHalf + firstHalf;
+            })
+            .reverse()
+            .join(' ');
+}
+
+fs.readFile(args[0], 'utf8', (err, data) => {
+  if (err) throw err;
+  const result = reverseVeryDisco(data.trim());
+  console.log(result);
+});
