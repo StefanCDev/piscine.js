712b9e670ce55b7ce1c92f8a86d3ee99c70a4a74
scornea <scornea3@gmail.com>
2023-03-25 23:10:46 +0000

    verydisco-reverso

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,16 +1,27 @@
-import { readFileSync } from 'fs';
+import fs from "fs";
 
-const discoChars = 'DSECORYIUAPFGHLJKLMNBQTVWXZ';
-const plainChars = 'DISCOVERYABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const reverseMap = new Map([...discoChars].map((c, i) => [c, plainChars[i]]));
-
-const reverseVeryDisco = content =>
-  content
-    .split('')
-    .map(c => reverseMap.has(c) ? reverseMap.get(c) : c)
-    .reverse()
-    .join('');
+function reverseVeryDisco(word) {
+  const len = word.length;
+  const halfLen = Math.floor(len / 2);
+  const firstHalf = word.slice(0, halfLen);
+  const secondHalf = word.slice(halfLen);
+  return secondHalf + firstHalf;
+}
 
 const filePath = process.argv[2];
-const content = readFileSync(filePath, 'utf8');
-console.log(reverseVeryDisco(content));
+
+if (filePath) {
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const words = data.split(" ");
+    const output = words.map(reverseVeryDisco);
+    const input = output.join(" ");
+    console.log(input);
+  });
+} else {
+  console.error("Please provide a file path argument");
+}
