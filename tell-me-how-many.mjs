2f790cff53c64dcbab735a88077eb4d5a67d4b85
scornea <scornea3@gmail.com>
2023-03-25 23:38:20 +0000

    tell-me-how-many

--- tell-me-how-many.mjs
+++ tell-me-how-many.mjs
@@ -2,7 +2,7 @@ import { readdir } from 'fs/promises';
 import { join } from 'path';
 
 // Get the directory path from the command line arguments
-const directoryPath = process.argv[2] || '/home/stefan/Stefan/01F.Projects/piscine-js-2weeks/tell-me-how-many/guests';
+const directoryPath = process.argv[2] || '/home/stefan/Stefan/01F.Projects/piscine-js-2weeks/guests';
 
 // Read the directory and count the entries
 async function countEntries() {
