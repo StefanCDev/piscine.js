9efa6e687949b75eaddab9aedd6e3b060e66d6ac
scornea <scornea3@gmail.com>
2023-03-27 09:00:55 +0100

    tell-me-how-many

--- tell-me-how-many.mjs
+++ tell-me-how-many.mjs
@@ -1,17 +1,22 @@
-import { readdir } from 'fs/promises';
-import { join } from 'path';
+// tell-me-how-many.mjs
+import fs from 'fs/promises';
+import process from 'process';
+import path from 'path';
 
-// Get the directory path from the command line arguments
-const directoryPath = process.argv[2] || '/home/stefan/Stefan/01F.Projects/piscine-js-2weeks/guests';
+// Main function to read the directory and count the number of entries
+async function main() {
+  // Get the directory path from the command line or use the current directory
+  const dirPath = process.argv.length > 2 ? process.argv[2] : '.';
 
-// Read the directory and count the entries
-async function countEntries() {
   try {
-    const entries = await readdir(directoryPath);
-    console.log(`There are ${entries.length} entries in ${directoryPath}`);
-  } catch (err) {
-    console.error(err);
+    const resolvedPath = path.resolve(dirPath);
+    const entries = await fs.readdir(resolvedPath);
+    const entryCount = entries.length;
+
+    console.log(entryCount);
+  } catch (error) {
+    console.error('Error reading directory:', error);
   }
 }
 
-countEntries();
+main();
