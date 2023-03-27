5145b8069512de298fda0e45fad0bf7e933f227f
scornea <scornea3@gmail.com>
2023-03-27 09:22:55 +0100

    tell-me-vip

---
+++ tell-me-vip.mjs
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+// Read the list of guests from a file or database.
+const guests = [
+  { firstname: 'Alice', lastname: 'Smith', response: 'YES' },
+  { firstname: 'Bob', lastname: 'Johnson', response: 'NO' },
+  { firstname: 'Charlie', lastname: 'Brown', response: 'YES' },
+  { firstname: 'David', lastname: 'Davis', response: 'MAYBE' },
+];
+
+// Filter the guests who answered 'YES' to the invitation.
+const vipGuests = guests.filter(guest => guest.response === 'YES');
+
+// Sort the filtered list in ascending order of the guests' last name.
+vipGuests.sort((a, b) => a.lastname.localeCompare(b.lastname));
+
+// Write the sorted list to a file named vip.txt.
+const fileStream = fs.createWriteStream('vip.txt');
+vipGuests.forEach((guest, index) => {
+  // Format each line of the file as "Number. Lastname Firstname".
+  const line = `${index + 1}. ${guest.lastname} ${guest.firstname}\n`;
+  fileStream.write(line);
+});
+fileStream.end();
